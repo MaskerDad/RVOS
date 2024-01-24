5482d5af9d7b9dada691cc1fe2d98572ab1e8259
zq <2689605766@qq.com>
2024-01-24 19:01:21 +0800

    fix bugs: abort to the TaskControlBlock::new

--- kernel/build.rs
+++ kernel/build.rs
@@ -1,15 +1,13 @@
-//! Dynamic generate a link_app.S by ../user
 use std::fs::{read_dir, File};
 use std::io::{Result, Write};
 
-static TARGET_PATH: &str = "../user/target/riscv64gc-unknown-none-elf/release/";
-
 fn main() {
-    println!("Unfortunately, RVOS does not currently implement file system functionality.");
-    println!("RVOS must link userland applications to kernel data segments!");
-    println!("These user_apps path: [{}]", TARGET_PATH);
+    println!("cargo:rerun-if-changed=../user/src/");
+    println!("cargo:rerun-if-changed={}", TARGET_PATH);
     insert_app_data().unwrap();
 }
+
+static TARGET_PATH: &str = "../user/target/riscv64gc-unknown-none-elf/release/";
 
 fn insert_app_data() -> Result<()> {
     let mut f = File::create("src/link_app.S").unwrap();
@@ -20,10 +18,10 @@ fn insert_app_data() -> Result<()> {
             let mut name_with_ext = dir_entry.unwrap().file_name().into_string().unwrap();
             name_with_ext.drain(name_with_ext.find('.').unwrap()..name_with_ext.len());
             name_with_ext
-        }).collect();
+        })
+        .collect();
     apps.sort();
-    
-    // app num
+
     writeln!(
         f,
         r#"
@@ -35,13 +33,11 @@ _num_app:
         apps.len()
     )?;
 
-    // app start address
     for i in 0..apps.len() {
         writeln!(f, r#"    .quad app_{}_start"#, i)?;
     }
     writeln!(f, r#"    .quad app_{}_end"#, apps.len() - 1)?;
 
-    // app data
     for (idx, app) in apps.iter().enumerate() {
         println!("app_{}: {}", idx, app);
         writeln!(
@@ -50,11 +46,12 @@ _num_app:
     .section .data
     .global app_{0}_start
     .global app_{0}_end
+    .align 3
 app_{0}_start:
-    .incbin "{2}{1}.bin"
+    .incbin "{2}{1}"
 app_{0}_end:"#,
             idx, app, TARGET_PATH
         )?;
     }
-    Ok(())    
+    Ok(())
 }
