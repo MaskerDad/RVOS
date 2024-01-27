42a7a5f303c1c10cb759347c4eb177557262ff81
zq <2689605766@qq.com>
2024-01-27 20:03:33 +0800

    process-os: user: enable user heap allocation

--- user/src/lib.rs
+++ user/src/lib.rs
@@ -1,15 +1,34 @@
 #![no_std]
 #![feature(linkage)]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
 #[macro_use]
 pub mod console;
 mod lang_items;
 mod syscall;
 
+use buddy_system_allocator::LockedHeap;
+use syscall::*;
+
+const USER_HEAP_SIZE: usize = 16384;
+static mut HEAP_SPACE: [u8; USER_HEAP_SIZE] = [0; USER_HEAP_SIZE];
+
+#[global_allocator]
+static HEAP: LockedHeap = LockedHeap::empty();
+
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:?}", layout);
+}
+
 #[no_mangle]
 #[link_section = ".text.entry"]
 pub extern "C" fn _start() -> ! {
+    unsafe {
+        HEAP.lock()
+            .init(HEAP_SPACE.as_ptr() as usize, USER_HEAP_SIZE);
+    }
     exit(main());
     panic!("unreachable after sys_exit!");
 }
@@ -19,8 +38,6 @@ pub extern "C" fn _start() -> ! {
 fn main() -> i32 {
     panic!("Cannot find main!");
 }
-
-use syscall::*;
 
 pub fn read(fd: usize, buf: &mut [u8]) -> isize {
     sys_read(fd, buf)
