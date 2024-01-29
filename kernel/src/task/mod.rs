b442418cf5615cb8f7f01548257a2685b748c309
zq <zhouquan1511@163.com>
2024-01-29 10:33:41 +0800

    process-os: add initproc to the TASK_MANAGER

--- kernel/src/task/mod.rs
+++ kernel/src/task/mod.rs
@@ -24,6 +24,10 @@ use manager::{add_task, fetch_task};
 use context::TaskContext;
 use switch::__switch;
 use pid::{pid_alloc, KernelStack, PidHandle};
+use processor::{
+    current_task, take_current_task, current_trap_cx, current_user_token,
+    run_tasks, schedule, Processor,
+};
 
 use lazy_static::*;
 use alloc:sync::Arc;
@@ -37,4 +41,21 @@ lazy_static! {
 ///Add init process to the TASK_MANAGER
 pub fn add_initproc() {
     add_task(INITPROC.clone());
+}
+
+///Suspend the current `Running` task and run the next task by `fetch_task`
+pub fn suspend_current_and_run_next() {
+    let task = take_current_task().unwrap();
+    
+    let mut task_inner = task.inner_exclusive_access();
+    task_inner.task_status = TaskStatus::Ready;
+    let switched_task_cx_ptr = &mut task_inner.task_cx as *mut TaskContext;
+    drop(task_inner);
+
+    add_task(task);
+    schedule(switched_task_cx_ptr);
+}
+
+pub fn exit_current_and_run_next(exit_code: i32) {
+    //TODO: reclaim
 }
